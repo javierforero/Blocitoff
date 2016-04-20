6cfa0f32ea5e86a3f5d4fa0473b62a76f8e11aa1
Javier Forero <javierforero01@gmail.com>
2016-04-20 07:26:03 -0500

    Added user controller assigns rspec

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,4 @@
 class ListsController < ApplicationController
-
   def show
     @list = List.find(params[:id])
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+  let(:my_user) { create(:user)}
+  describe "finds user" do
+    
+    it "assigns @user" do
+      get :show, {id: my_user.id}
+      expect(assigns(:user)).to eq my_user
+    end
+  end
 end
