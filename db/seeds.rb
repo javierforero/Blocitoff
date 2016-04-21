a9320586e90fb17ed28e97d7150219bdea40f1f6
Javier Forero <javierforero01@gmail.com>
2016-04-21 08:23:05 -0500

    blocitoff 6. still have to test other users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+
+
+50.times do
+  pw = Faker::Internet.password(8)
+  User.create!(
+
+  name: Faker::Name.name,
+  email: Faker::Internet.email,
+  password: pw,
+  password_confirmation: pw,
+  confirmed_at: Faker::Date.backward(16)
+  )
+end
+
+users = User.all
+
+100.times do
+
+  Item.create!(
+  name: Faker::Lorem.sentence,
+  user: users.sample
+  )
+end
+
+puts "seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
